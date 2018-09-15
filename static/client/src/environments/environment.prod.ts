2585235f1af1071987d1cfad99331897813749e0
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-09-15 11:01:20 +0000

    Prod/staging parameters

--- static/client/src/environments/environment.prod.ts
+++ static/client/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  self: 'http://46.101.7.84:9080',
+  self: 'http://46.101.183.51:9080',
   storage: {
     keyName: 'currentUser'
   },
   services: {
     bind: {
-      apiUrl: 'http://46.101.7.84:9081/api/v1'
+      apiUrl: 'http://46.101.183.51:9081/api/v1'
     }
   }
 };
